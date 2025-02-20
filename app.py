4b20608eb8f473b44a16d4fc848069e02d473afb
Manmadha <manmadhasangati99@gmail.com>
2025-02-20 11:52:16 +0530

    first commit

--- app.py
+++ app.py
@@ -23,4 +23,48 @@ def create_connection():
         st.error(f"Error connecting to database: {e}")
     return None
 
+def create_table():
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS account (
+        id INT AUTO_INCREMENT primary key,
+        firstName VARCHAR(255),
+        lastName VARCHAR(255),
+        email VARCHAR(255) NOT NULL UNIQUE,
+        phone VARCHAR(20),
+        age INT,
+        message LONGTEXT
+    );
+    """
+    conn = create_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(create_table_query)
+            conn.commit()
+        except Error as e:
+            st.error(f"Error creating table: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+def insert_data(firstName, lastName, email, phone, age, message):
+    conn = create_connection()
+    if conn:
+        try:
+            insert_query = """
+            INSERT INTO account (firstName, lastName, email, phone, age, message)
+            VALUES (%s, %s, %s, %s, %s, %s);
+            """
+            cursor = conn.cursor()
+            cursor.execute(insert_query, (firstName, lastName, email, phone, age, message))
+            conn.commit()
+            return True
+        except Error as e:
+            st.error(f"Error inserting data: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+    return False
+
+
 
